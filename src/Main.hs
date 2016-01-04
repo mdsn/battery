65f58d8ae1b8685e68632a8026229922672f9655
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2016-01-04 20:35:51 -0300

    Main.hs: load files using readFile

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,18 @@
 
 module Main where
 
-import System.Process
-
 currentChargePath = "/sys/class/power_supply/BAT1/charge_now"
 fullChargePath    = "/sys/class/power_supply/BAT1/charge_full"
 
 main :: IO ()
 main = do
-    currentStr <- readProcess "cat" [currentChargePath] ""
-    fullStr    <- readProcess "cat" [fullChargePath] ""
+    currentStr <- readFile currentChargePath
+    fullStr    <- readFile fullChargePath
 
     let current    = read currentStr :: Integer
         full       = read fullStr    :: Integer
         percentage = current * 100 `div` full
 
     putStrLn $ show percentage
-    
+
+
